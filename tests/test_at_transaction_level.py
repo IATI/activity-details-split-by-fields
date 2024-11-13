42c4ecd1ec6a8f8104706259d92bd8b1f7e061ef
James B <james.baster@opendataservices.coop>
2024-11-13 10:01:44 +0000

    tests: Add more tests, and add TODO note with more tests needed for bad data

--- tests/test_at_transaction_level.py
+++ tests/test_at_transaction_level.py
@@ -1,1 +1,48 @@
-# TODO When country, sector, etc specified on transaction directly, no splits should occur. Test that here
+from iati_activity_details_split_by_fields.iati_activity import IATIActivity
+from iati_activity_details_split_by_fields.iati_activity_transaction import (
+    IATIActivityTransaction,
+)
+from iati_activity_details_split_by_fields.iati_activity_transaction_sector import (
+    IATIActivityTransactionSector,
+)
+
+
+def test_country_set():
+
+    iati_activity = IATIActivity(
+        transactions=[IATIActivityTransaction(value=1000, recipient_country_code="GB")],
+    )
+
+    results = iati_activity.get_transactions_split_as_json()
+
+    assert [
+        {
+            "recipient_country_code": "GB",
+            "sectors": [],
+            "value": 1000,
+        }
+    ] == results
+
+
+def test_sector_set():
+
+    iati_activity = IATIActivity(
+        transactions=[
+            IATIActivityTransaction(
+                value=1000,
+                sectors=[
+                    IATIActivityTransactionSector(vocabulary="cats", code="Henry")
+                ],
+            )
+        ],
+    )
+
+    results = iati_activity.get_transactions_split_as_json()
+
+    assert [
+        {
+            "recipient_country_code": None,
+            "sectors": [{"code": "Henry", "vocabulary": "cats"}],
+            "value": 1000,
+        },
+    ] == results
---
+++ tests/test_both_at_activity_and_transaction_level.py
@@ -0,0 +1,4 @@
+# TODO
+# What happens when a country, region, sector is set at BOTH Activity and Transaction?
+# According to standard, this isn't allowed!
+# But we should test this to make sure bad data is ok
